36477009da03e4d7fbe11d39bbfcf2f4f7afb08a
Will Thames <wthames@skedulo.com>
2020-09-03 17:28:28 +1000

    Escape multiline separators
    Otherwise the output gets truncated after the first line break
    
    See https://github.community/t/set-output-truncates-multiline-strings/16852/5

--- action.sh
+++ action.sh
@@ -3,4 +3,8 @@ set -eu
 
 result="$(/bin/ansible-inventory-diff.sh --dir /github/workspace $@)"
 echo "$result"
+result="${result//'%'/'%25'}"
+result="${result//$'\n'/'%0A'}"
+result="${result//$'\r'/'%0D'}"
+
 echo "::set-output name=result::$result"
